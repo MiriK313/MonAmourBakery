81cb17c0502da5d6d7f681939f8efa630d8e97e4
stavf <stavfinz@gmail.com>
2021-02-22 17:36:07 +0200

    fourth commit MONAMOUR

--- app/src/main/java/com/example/monamourbakery/Fragment_Cart.java
+++ app/src/main/java/com/example/monamourbakery/Fragment_Cart.java
@@ -1,6 +1,8 @@
 package com.example.monamourbakery;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -27,10 +30,11 @@ public class Fragment_Cart extends Fragment_Base{
     public RecyclerView products_LST;
     private RadioGroup delivery_orders_GRP;
     private Button delivery_button_orders_RDB;
-    private EditText delivery_address_orders_TXT;
-    private DatePicker date_orders_calc;
+    private MaterialTextView special_delivery_note_TXT;
+    private DatePickerDialog datePickerDialog;
+    private EditText date_EDT;
     //price
-    private MaterialTextView total_price_orders_LBL;
+    private TextView total_price_orders_LBL;
     private MaterialButton pay_orders_BTN;
 
     private Order currentOrder;
@@ -42,7 +46,7 @@ public class Fragment_Cart extends Fragment_Base{
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_current_order, container, false);
         // special_product=new Product();
-
+        //date_EDT.setInputType(InputType.TYPE_NULL);
         findViews(view);
         initViews(view);
 
@@ -78,24 +82,44 @@ public class Fragment_Cart extends Fragment_Base{
 
                 Log.d("DDM", "id" + checkedId);
                 if (checkedId == R.id.delivery_yes_orders_RDB) {
-                    delivery_address_orders_TXT.setVisibility(View.VISIBLE);
+                    special_delivery_note_TXT.setVisibility(View.VISIBLE);
                     //TODO add the delivery order address
                     //TODO add delivery price to total price
                 } else if (checkedId == R.id.delivery_no_orders_RDB) {
-                    Log.d("DDM", "Institute: fur");
+                    Log.d("DDM", "Institute: no");
+                    special_delivery_note_TXT.setVisibility(View.INVISIBLE);
                 }
             }
         });
+        date_EDT.setInputType(InputType.TYPE_NULL);
+        date_EDT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(v.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                date_EDT.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                Log.d("DDM", "Date: "+date_EDT.getText());
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
 
-
-        int currentMonth = Calendar.getInstance().get(Calendar.MONTH)+1;
-        int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        if (date_orders_calc.getDayOfMonth()>currentDay &&  date_orders_calc.getMonth()>=currentMonth && date_orders_calc.getYear()>=currentYear)
-        {
-            Log.d("pttt","Selected Date: "+ date_orders_calc.getDayOfMonth()+"/"
-                    + (date_orders_calc.getMonth() + 1)+"/"+date_orders_calc.getYear());
-        }
+//        int currentMonth = Calendar.getInstance().get(Calendar.MONTH)+1;
+//        int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+//        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+//        if (date_orders_calc.getDayOfMonth()>currentDay &&  date_orders_calc.getMonth()>=currentMonth && date_orders_calc.getYear()>=currentYear)
+//        {
+//            Log.d("pttt","Selected Date: "+ date_orders_calc.getDayOfMonth()+"/"
+//                    + (date_orders_calc.getMonth() + 1)+"/"+date_orders_calc.getYear());
+//        }
 
         pay_orders_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,10 +133,10 @@ public class Fragment_Cart extends Fragment_Base{
     private void findViews(View view) {
 
         delivery_orders_GRP=view.findViewById(R.id.delivery_orders_GRP);
-        delivery_address_orders_TXT=(EditText)view.findViewById(R.id.delivery_address_orders_TXT);
+        special_delivery_note_TXT=view.findViewById(R.id.special_delivery_note_TXT);
         products_LST=view.findViewById(R.id.products_LST);
         //price
-        date_orders_calc=view.findViewById(R.id.date_orders_calc);
+        date_EDT=(EditText) view.findViewById(R.id.date_EDT);
         total_price_orders_LBL=view.findViewById(R.id.total_price_orders_LBL);
         pay_orders_BTN= (MaterialButton) view.findViewById(R.id.pay_orders_BTN);
 
